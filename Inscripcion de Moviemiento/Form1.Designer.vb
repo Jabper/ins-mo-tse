5d82176dc9b146361ebceb45aa65e7c992d8a7d3
raul_teruel@asiconsultant.com <raul_teruel@asiconsultant.com@dbb76f3b-6ef1-ec56-c1ea-d0c1eac798ce>
2012-05-29 16:29:13 +0000

    hola

--- Inscripcion de Moviemiento/Form1.Designer.vb
+++ Inscripcion de Moviemiento/Form1.Designer.vb
@@ -22,9 +22,49 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.XtraTabControl1 = New DevExpress.XtraTab.XtraTabControl
+        Me.XtraTabPage1 = New DevExpress.XtraTab.XtraTabPage
+        Me.XtraTabPage2 = New DevExpress.XtraTab.XtraTabPage
+        CType(Me.XtraTabControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XtraTabControl1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'XtraTabControl1
+        '
+        Me.XtraTabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.XtraTabControl1.Name = "XtraTabControl1"
+        Me.XtraTabControl1.SelectedTabPage = Me.XtraTabPage1
+        Me.XtraTabControl1.Size = New System.Drawing.Size(300, 300)
+        Me.XtraTabControl1.TabIndex = 0
+        Me.XtraTabControl1.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XtraTabPage1, Me.XtraTabPage2})
+        '
+        'XtraTabPage1
+        '
+        Me.XtraTabPage1.Name = "XtraTabPage1"
+        Me.XtraTabPage1.Size = New System.Drawing.Size(294, 272)
+        Me.XtraTabPage1.Text = "XtraTabPage1"
+        '
+        'XtraTabPage2
+        '
+        Me.XtraTabPage2.Name = "XtraTabPage2"
+        Me.XtraTabPage2.Size = New System.Drawing.Size(0, 0)
+        Me.XtraTabPage2.Text = "XtraTabPage2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 262)
+        Me.Controls.Add(Me.XtraTabControl1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.XtraTabControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XtraTabControl1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents XtraTabControl1 As DevExpress.XtraTab.XtraTabControl
+    Friend WithEvents XtraTabPage1 As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XtraTabPage2 As DevExpress.XtraTab.XtraTabPage
 
 End Class
