ab7944023cc64dcea9ed5d9c6303547594695d3e
eden_lazo@asiconsultant.com <eden_lazo@asiconsultant.com@dbb76f3b-6ef1-ec56-c1ea-d0c1eac798ce>
2012-06-11 17:37:26 +0000

    INICIO DEL DESARROLLO PARA XfrmIngresoCandidatos Y ACTUALIZACION DEL LLAMADO EN XFrmMenuPrincipal

--- trunk/Inscripcion de Moviemiento/My Project/Settings.Designer.vb
+++ trunk/Inscripcion de Moviemiento/My Project/Settings.Designer.vb
@@ -64,6 +64,17 @@ Namespace My
                 Return CType(Me("ConnectionString"),String)
             End Get
         End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=192.168.1.154:1521;Persist Security Info=True;User ID=tse;Password=or"& _ 
+            "acle;Unicode=True")>  _
+        Public ReadOnly Property ConnectionString1() As String
+            Get
+                Return CType(Me("ConnectionString1"),String)
+            End Get
+        End Property
     End Class
 End Namespace
 
