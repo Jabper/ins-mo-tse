6c13155b4f8bf239c61675a8187bbc3a42758654
eden_lazo@asiconsultant.com <eden_lazo@asiconsultant.com@dbb76f3b-6ef1-ec56-c1ea-d0c1eac798ce>
2012-06-22 14:54:48 +0000

    Seleccionar Datos

---
+++ trunk/Inscripcion de Moviemiento/Candidatos/xfrmSeleccionarPartidoMovimiento.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class xfrmSeleccionarPartidoMovimiento
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnCrear = New DevExpress.XtraEditors.SimpleButton
+        Me.BtnSalir = New DevExpress.XtraEditors.SimpleButton
+        Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl
+        Me.CmbPartido = New DevExpress.XtraEditors.LookUpEdit
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.CmbMovimiento = New DevExpress.XtraEditors.LookUpEdit
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupControl1.SuspendLayout()
+        CType(Me.CmbPartido.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CmbMovimiento.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnCrear
+        '
+        Me.BtnCrear.Appearance.Font = New System.Drawing.Font("Tahoma", 7.0!, System.Drawing.FontStyle.Bold)
+        Me.BtnCrear.Appearance.Options.UseFont = True
+        Me.BtnCrear.Image = Global.Inscripcion_de_Moviemientos.My.Resources.Resources.agregar
+        Me.BtnCrear.ImageLocation = DevExpress.XtraEditors.ImageLocation.TopCenter
+        Me.BtnCrear.Location = New System.Drawing.Point(179, 117)
+        Me.BtnCrear.Name = "BtnCrear"
+        Me.BtnCrear.Size = New System.Drawing.Size(55, 55)
+        Me.BtnCrear.TabIndex = 29
+        Me.BtnCrear.Text = "Crear"
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Appearance.Font = New System.Drawing.Font("Tahoma", 7.0!, System.Drawing.FontStyle.Bold)
+        Me.BtnSalir.Appearance.Options.UseFont = True
+        Me.BtnSalir.Image = Global.Inscripcion_de_Moviemientos.My.Resources.Resources.im_aim
+        Me.BtnSalir.ImageLocation = DevExpress.XtraEditors.ImageLocation.TopCenter
+        Me.BtnSalir.Location = New System.Drawing.Point(285, 117)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(55, 55)
+        Me.BtnSalir.TabIndex = 28
+        Me.BtnSalir.Text = "Salir"
+        '
+        'GroupControl1
+        '
+        Me.GroupControl1.Controls.Add(Me.CmbPartido)
+        Me.GroupControl1.Controls.Add(Me.Label4)
+        Me.GroupControl1.Controls.Add(Me.Label5)
+        Me.GroupControl1.Controls.Add(Me.CmbMovimiento)
+        Me.GroupControl1.Location = New System.Drawing.Point(28, 33)
+        Me.GroupControl1.Name = "GroupControl1"
+        Me.GroupControl1.Size = New System.Drawing.Size(476, 78)
+        Me.GroupControl1.TabIndex = 27
+        Me.GroupControl1.Text = "Información General"
+        '
+        'CmbPartido
+        '
+        Me.CmbPartido.Location = New System.Drawing.Point(88, 29)
+        Me.CmbPartido.Name = "CmbPartido"
+        Me.CmbPartido.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.CmbPartido.Properties.Columns.AddRange(New DevExpress.XtraEditors.Controls.LookUpColumnInfo() {New DevExpress.XtraEditors.Controls.LookUpColumnInfo("CODIGO_PARTIDO", "Código", 40, DevExpress.Utils.FormatType.Numeric, "", True, DevExpress.Utils.HorzAlignment.Far, DevExpress.Data.ColumnSortOrder.Ascending), New DevExpress.XtraEditors.Controls.LookUpColumnInfo("NOMBRE", "Partido", 100, DevExpress.Utils.FormatType.None, "", True, DevExpress.Utils.HorzAlignment.Near)})
+        Me.CmbPartido.Properties.DisplayMember = "NOMBRE"
+        Me.CmbPartido.Properties.NullText = "Seleccione"
+        Me.CmbPartido.Properties.ValueMember = "CODIGO_PARTIDO"
+        Me.CmbPartido.Size = New System.Drawing.Size(135, 20)
+        Me.CmbPartido.TabIndex = 20
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(5, 32)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 13)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Partido Político"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(242, 32)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 13)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Movimiento"
+        '
+        'CmbMovimiento
+        '
+        Me.CmbMovimiento.Location = New System.Drawing.Point(309, 29)
+        Me.CmbMovimiento.Name = "CmbMovimiento"
+        Me.CmbMovimiento.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.CmbMovimiento.Properties.Columns.AddRange(New DevExpress.XtraEditors.Controls.LookUpColumnInfo() {New DevExpress.XtraEditors.Controls.LookUpColumnInfo("NOMBRE_MOVIMIENTO", "Movimiento", 123, DevExpress.Utils.FormatType.None, "", True, DevExpress.Utils.HorzAlignment.Near, DevExpress.Data.ColumnSortOrder.Ascending)})
+        Me.CmbMovimiento.Properties.DisplayMember = "NOMBRE_MOVIMIENTO"
+        Me.CmbMovimiento.Properties.NullText = "Seleccione"
+        Me.CmbMovimiento.Properties.ValueMember = "CODIGO_MOVIMIENTO"
+        Me.CmbMovimiento.Size = New System.Drawing.Size(144, 20)
+        Me.CmbMovimiento.TabIndex = 4
+        '
+        'xfrmSeleccionarPartidoMovimiento
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(528, 201)
+        Me.Controls.Add(Me.BtnCrear)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.GroupControl1)
+        Me.Name = "xfrmSeleccionarPartidoMovimiento"
+        Me.Text = "Seleccion los Datos"
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupControl1.ResumeLayout(False)
+        Me.GroupControl1.PerformLayout()
+        CType(Me.CmbPartido.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CmbMovimiento.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BtnCrear As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnSalir As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GroupControl1 As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents CmbPartido As DevExpress.XtraEditors.LookUpEdit
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents CmbMovimiento As DevExpress.XtraEditors.LookUpEdit
+End Class
---
+++ trunk/Inscripcion de Moviemiento/Candidatos/xfrmSeleccionarPartidoMovimiento.vb
@@ -0,0 +1,3 @@
+﻿Public Class xfrmSeleccionarPartidoMovimiento 
+
+End Class
