1a853f22a2f2dbef5b9160635fc65a5076e088c0
eden_lazo@asiconsultant.com <eden_lazo@asiconsultant.com@dbb76f3b-6ef1-ec56-c1ea-d0c1eac798ce>
2012-06-22 14:54:48 +0000

    Seleccionar Datos

--- Inscripcion de Moviemiento/Consultas/XfrmJerarquico.vb
+++ Inscripcion de Moviemiento/Consultas/XfrmJerarquico.vb
@@ -2,8 +2,8 @@
 
     Private Sub XfrmJerarquico_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'DSConsultas.Departamentos' table. You can move, or remove it, as needed.
-        Me.DepartamentosTableAdapter.Fill(Me.DSConsultas.Departamentos)
-        Me.MunicipiosTableAdapter.Fill(Me.DSConsultas.Municipios)
+        'Me.DepartamentosTableAdapter.Fill(Me.DSConsultas.Departamentos)
+        'Me.MunicipiosTableAdapter.Fill(Me.DSConsultas.Municipios)
 
     End Sub
 End Class
